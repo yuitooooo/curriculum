12211af6eac53b8001e84992455e7046f3d9e9bd
yuitooooo <yuito.sbaseboll@gmail.com>
2023-01-22 17:46:36 +0900

    Post delete

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,15 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use HasFactory;
+    use SoftDeletes;
+    
     protected $fillable = [
         'title',
         'body',
     ];
     
-    use HasFactory;
+   
 
     public function getPaginateByLimit(int $limit_count = 5)
     {
